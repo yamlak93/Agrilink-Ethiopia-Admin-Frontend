27183368628ddd779c79ae52afa8fb0346e2ac02
Yamlak Firew <yamlak93@gmail.com>
2025-12-14 10:59:43 +0300

    Changing the backend url to http://192.168.100.72:5000/api

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5000/api/admin', // Keep as is, matches index.js
+  baseURL: 'http://192.168.100.72:5000/api/admin', // Keep as is, matches index.js
 });
 
 apiClient.interceptors.request.use(
