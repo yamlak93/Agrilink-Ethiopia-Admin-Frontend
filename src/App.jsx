3fbdf9249cce17062ea53ed109ea2f1dd4aa32c4
Yamlak Firew <yamlak93@gmail.com>
2025-12-14 10:28:57 +0300

    Adding page not found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
+import { Navigate } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
@@ -19,12 +18,14 @@ import LoginPage from "./auth/LoginPage";
 import RegisterPage from "./auth/RegisterPage";
 import ForgotPasswordPage from "./auth/ForgotPasswordPage";
 import FarmerBankAccountPage from "./pages/FarmerBankAccountPage";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
+          <Route path="/" element={<Navigate to="/admin/login" replace />} />
           <Route path="/admin/login" element={<LoginPage />} />
           <Route path="/admin/register" element={<RegisterPage />} />
           <Route
@@ -44,6 +45,7 @@ function App() {
           <Route path="/admin/analytics" element={<AdminAnalytics />} />
           <Route path="/admin/reports" element={<Reports />} />
           <Route path="/admin/payments" element={<PaymentsPage />} />
+          <Route path="*" element={<PageNotFound />} />
           <Route
             path="/admin/manage-banks-accounts"
             element={<FarmerBankAccountPage />}
